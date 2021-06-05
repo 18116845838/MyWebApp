1ec94405d6ca44d21d52f114761fdf60aef399da
2811733392@qq.com <2811733392@qq.com>
2021-06-05 15:23:32 +0800

    添加js文件MyJsFile,JsHomework完成js变量赋值作业

---
+++ MyWebApp/wwwroot/js/MyJSFile .js
@@ -0,0 +1,13 @@
+﻿////分别用HTML页面内嵌和.js文件引用的方式，弹出你的入栈口号。
+////声明一个变量sname，表示同学的姓名；通过更改变量值，弹出至少3个同学的姓名
+////配置Visual Studio，让其能自动检测从你Javascript中不规范 / 错误的代码
+var slogan = "键盘敲烂,月薪过万!";
+alert(slogan);
+var sname = "张志民";
+alert(sname);
+
+sname = "周丁浩";
+alert(sname);
+
+sname = "杨家栋";
+alert(sname);
