14587f4225db8c049efacef7280f3e75661ac6f2
2811733392@qq.com <2811733392@qq.com>
2021-06-05 15:56:30 +0800

    声明problem对象,其中包含title、body、reward、needRemote等属性

--- MyWebApp/wwwroot/js/MyJSFile .js
+++ MyWebApp/wwwroot/js/MyJSFile .js
@@ -11,3 +11,10 @@ alert(sname);
 
 sname = "杨家栋";
 alert(sname);
+//观察“一起帮”的求助，定义一个problem对象，其中包含title、body、reward、needRemote等属性 
+var problem = {
+    title: null,
+    body: null,
+    reward: null,
+    needRemote: null
+}
