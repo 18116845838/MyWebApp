eca9daf68e3b4c320a89236d2f7d32d9da6ceb4b
2811733392@qq.com <2811733392@qq.com>
2021-06-06 15:44:54 +0800

    将每天/课的作业用不同的js文件分隔:分支branch，循环circulation

---
+++ MyWebApp/wwwroot/js/Circulation.js
@@ -0,0 +1,36 @@
+﻿
+//显示一段数中不重复的值（仔细思考需求）
+
+/*var array = [1, 2, 11, 1, true, "", '', " ", ' ', ' ', " ", true, '123', '123', "1234", "1234"];
+
+for (var i = 0; i < array.length; i++) {
+    for (var j = i+1; j < array.length; j++) {
+        if (array[i]===array[j]) {
+            array.splice(j, 1);
+            j--;
+        }
+    }
+}
+console.log(array);
+*/
+/*var rows = 5, columns = rows * 2 - 1;
+var yh = [];
+for (var i = 0; i < rows; i++) {
+    yh[i] = [];
+    var lines = '';
+    for (var j = 0; j < columns; j++) {
+        if (i===0) {
+            yh[i][j] = j === rows - 1 ? 1 : ' ';
+        } else {
+            var perRows = i - 1,
+                perColumns = j - 1 >= 0 ? +yh[perRows][j - 1] :0,
+                nextColunms = j + 1 < columns - 1 ? +yh[perRows][j + 1] : 0;
+           
+            var current = perColumns + nextColunms;
+            yh[i][j] = current  ? current:" ";
+        }
+        lines += yh[i][j];
+    }
+
+    console.log(lines);
+}*/
---
+++ MyWebApp/wwwroot/js/branch.js
@@ -0,0 +1,46 @@
+﻿//写一段代码，能根据一起帮用户的帮帮点（bCredit）输出他对应的等级（可适度精简）
+/*var bCredit = NaN;
+var grade;
+if (isNaN(bCredit)) {
+    grade = "错误";
+} else {
+    if (bCredit <= 10) {
+        grade = "入门";
+    } else if (bCredit <= 20) {
+        grade = "聚气";
+
+    } else if (bCredit <= 30) {
+        grade = "凝水";
+
+    } else if (bCredit <= 50) {
+        grade = "萌芽";
+
+    } else {
+        grade = "单叶"
+    }
+}
+console.log(grade);*/
+//用switch表示一起帮身份代号
+/*var user;
+var rank;
+switch (user) {
+    case 0:
+        rank = "访客";
+        break;
+    case 1:
+        rank = "注册用户";
+        break;
+    case 2:
+        rank = "文章发布者";
+        break;
+    case 3:
+        rank = "管理员";
+        break;
+    case 4:
+        rank = "超级管理员";
+        break;
+    default:
+        rank = "身份有误";
+        break;
+}
+console.log(rank);*/
