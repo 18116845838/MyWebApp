b01d07a5236727ddf6cddf7a7b4972f28b1573a4
2811733392@qq.com <2811733392@qq.com>
2021-06-07 09:00:26 +0800

    建立一个函数getMaxNumber(),并找出里面最大的数

--- MyWebApp/wwwroot/js/Function.js
+++ MyWebApp/wwwroot/js/Function.js
@@ -52,6 +52,7 @@ console.log(get986(has9(19, 29, 39, 4), has8(1, 2, 5, 18, 9), has6(1, 6, 16, 5))
 
 
 
+
 //将之前“找出素数”的代码封装成一个函数findPrime(max) ，可以打印出max以内的所有素数。
 
 
@@ -72,7 +73,7 @@ console.log(get986(has9(19, 29, 39, 4), has8(1, 2, 5, 18, 9), has6(1, 6, 16, 5))
 findPrime(101);*/
 
 //自行设计参数，将之前“累加求和”的代码封装成一个函数Sum() ，可以计算任意起始位置、任意步长（如：1, 3, 5……或者0, 5, 10, 15……）的等差数列之和。
-function sum(startNumber, stepSize, endNumber) {
+/*function sum(startNumber, stepSize, endNumber) {
     var number = 0;
 
     for (var i = startNumber; i < endNumber; i += stepSize) {
@@ -85,4 +86,19 @@ function sum(startNumber, stepSize, endNumber) {
     }
     return number;
 }
-console.log(sum(2, 49, 100));
+console.log(sum(2, 49, 100));*/
+//封装一个函数，建立一个函数getMaxNumber() ，可以接受任意多各种类型的参数，并找出里面最大的数（忽略数值以外的其他类型） 
+function getMaxNumber() {
+    var MaxNumber = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        if (typeof(arguments[i])==="number") {
+            if (arguments[i]>MaxNumber) {
+                MaxNumber = arguments[i];
+            }//else
+        } //else 
+
+    }
+    return MaxNumber;
+}
+console.log(getMaxNumber(9,[],17+3,NaN,"100",undefined,99, 3, 4, 7));
+
