0d582fb7b54d3dd143f205365243b691fd0d50ef
2811733392@qq.com <2811733392@qq.com>
2021-06-07 10:07:08 +0800

    将“冒泡排序”封装成函数bubbleSort() 和封装函数deleteDuplicated()

--- MyWebApp/wwwroot/js/Function.js
+++ MyWebApp/wwwroot/js/Function.js
@@ -88,7 +88,7 @@ findPrime(101);*/
 }
 console.log(sum(2, 49, 100));*/
 //封装一个函数，建立一个函数getMaxNumber() ，可以接受任意多各种类型的参数，并找出里面最大的数（忽略数值以外的其他类型） 
-function getMaxNumber() {
+/*function getMaxNumber() {
     var MaxNumber = 0;
     for (var i = 0; i < arguments.length; i++) {
         if (typeof(arguments[i])==="number") {
@@ -100,5 +100,38 @@ function getMaxNumber() {
     }
     return MaxNumber;
 }
-console.log(getMaxNumber(9,[],17+3,NaN,"100",undefined,99, 3, 4, 7));
+console.log(getMaxNumber(9,[],17+3,NaN,"100",undefined,99, 3, 4, 7));*/
 
+//封装一个函数swap(arr, i, j) ，可以交换数组arr里下标 i 和 j 的值
+function swap(arr, i, j) {
+    var temp;
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+var arr = [7,1, 2,1,3,4,7,5,6,3,];
+//console.log(swap(arr, 0, 1));
+
+//利用上面的swap()函数，将“冒泡排序”封装成函数bubbleSort() 
+function bubbleSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr.length-1-i; j++) {
+            if (arr[j]>arr[j+1]) {
+                swap(arr, j, j + 1);
+            }//else=
+        }
+    }
+}
+bubbleSort(arr);
+//封装函数deleteDuplicated()删除一个数组里面重复的元素
+function deleteDuplicated(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                arr.splice(j, 1);
+                j--;
+            }
+        }
+    }
+}
+deleteDuplicated(arr);
