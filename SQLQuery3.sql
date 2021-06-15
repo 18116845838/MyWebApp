f060b8039116d415e4cf971ee07076b543642356
2811733392@qq.com <2811733392@qq.com>
2021-06-15 16:58:34 +0800

    数据库1717bang的新建备份删除和恢复

---
+++ SQLQuery3.sql
@@ -0,0 +1,9 @@
+﻿--新建一个数据库：17bang，能指定/查看该数据库存放位置 
+CREATE DATABASE [17bang];
+--依次备份/删除/恢复该数据库 --
+BACKUP DATABASE [17bang] TO DISK='D:\\yz\\17bang.bak';
+
+DROP DATABASE [17bang];
+
+RESTORE DATABASE [17bang] FROM DISK='D:\\yz\\17bang.bak';
+
