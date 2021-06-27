fd2e42da08f4a9ca075632e83f7dfbc1dd0bb2d1
2811733392@qq.com <2811733392@qq.com>
2021-06-27 16:03:09 +0800

    无用的提交

--- ADOConsole/Program.cs
+++ ADOConsole/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 namespace ADOConsole
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			
 		}
 	}
 }
