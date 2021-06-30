bddbfa02471ee08d4fe285a64aff0d466485340e
2811733392@qq.com <2811733392@qq.com>
2021-06-30 16:49:49 +0800

    没有用的提交--删除了一些代码

---
+++ ADOConsole/DBSqlContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADOConsole
+{
+	class DBSqlContext :DbContext
+	{
+		//public DbSet<student> Students { get;set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder dbContext )
+		{ 
+			string connectonStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=17bang;Integrated Security=True;";
+			dbContext.UseSqlServer(connectonStr);
+			base.OnConfiguring(dbContext);
+
+		}
+	}
+}
