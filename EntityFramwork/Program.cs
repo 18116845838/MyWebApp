e8bf9eaf2f04d45c67d3479cbb49dbae2f522c7f
2811733392@qq.com <2811733392@qq.com>
2021-07-05 16:48:23 +0800

    重新提交  EF6建库建表--添加新的项目EntityFarmwork

---
+++ EntityFramwork/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramwork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//ConsructDb();
+
+
+			Console.ReadKey();
+
+		}
+		static void ConsructDb()
+		{
+			SqlDbContext dbContext = new SqlDbContext();
+			dbContext.Database.Delete();
+			dbContext.Database.Create();
+		}
+	}
+}
