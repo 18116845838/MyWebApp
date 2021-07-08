da2486c435b726963532bf2577896796407283dc
2811733392@qq.com <2811733392@qq.com>
2021-07-08 14:34:12 +0800

    将之前的静态HTML页面移动到Pages文件夹下

---
+++ MyWebApp/Pages/Profile/Write.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages.Profile
+{
+    public class WriteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
