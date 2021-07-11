8bb02ebaf89b2cb739903d81a17d0e1a27531aa8
2811733392@qq.com <2811733392@qq.com>
2021-07-11 16:04:33 +0800

    Register页面使用Tag Helper模仿表单内容

--- MyWebApp/Pages/Entity/User.cs
+++ MyWebApp/Pages/Entity/User.cs
@@ -1,8 +1,16 @@
-﻿namespace MyWebApp.Pages.Article
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWebApp.Pages.Article
 {
-	public class User:Entity
+	public class User : Entity
 	{
 		public string Name { get; set; }
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+		[DataType(DataType.Password)]
+		public string ValidatePassword { get; set; }
+		public string Captcha { get; set; }
 		public int Age { get; set; }
+		public string InvitationCode { get; set; }
 	}
 }
--- MyWebApp/Pages/FAQ/Register.cshtml.cs
+++ MyWebApp/Pages/FAQ/Register.cshtml.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyWebApp.Pages.Article;
 
 namespace MyWebApp.Pages.FAQ
 {
     public class RegisterModel : PageModel
     {
+        public User NewUser { get; set; }
         public void OnGet()
         {
         }
