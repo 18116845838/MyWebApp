1d3ba15b66a7b1f8345b90810570210a8c05b14a
2811733392@qq.com <2811733392@qq.com>
2021-07-16 20:59:47 +0800

    在MVC项目上完成求督促页面

---
+++ 17bangMVC/Controllers/PlanNewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _17bangMVC.Controllers
+{
+    public class PlanNewController : Controller
+    {
+        // GET: PlanNew
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
