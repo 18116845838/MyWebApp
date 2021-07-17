781dd8bf64026c240898cfc8542e203aea9490ac
2811733392@qq.com <2811733392@qq.com>
2021-07-17 16:17:50 +0800

    在MVC项目中用HtmlHelper生成PlanNew页面form控件

--- 17bangMVC/Controllers/PlanNewController.cs
+++ 17bangMVC/Controllers/PlanNewController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _17bangMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,36 @@ using System.Web.Mvc;
 
 namespace _17bangMVC.Controllers
 {
-    public class PlanNewController : Controller
-    {
-        // GET: PlanNew
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class PlanNewController : Controller
+	{
+		// GET: PlanNew
+
+		public ActionResult Index()
+		{
+			PlanNewModel plan = new PlanNewModel
+			{
+				DaysInWeek = new Dictionary<string, bool>
+				{
+					{ "一",true},
+					{ "二",true},
+					{ "三",true},
+					{ "四",true},
+					{ "五",true},
+					{ "六",true},
+					{ "七",true}
+				},
+				DaysOfLeaveOptions = new Dictionary<int, int>
+				{
+					{1,1 },
+					{2,2 },
+					{3,3 },
+					{4,4 },
+					{5,5 },
+					{6,6 },
+					{7,7 },
+				}
+			};
+			return View(plan);
+		}
+	}
 }
---
+++ 17bangMVC/Models/PlanNewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _17bangMVC.Models
+{
+	public class PlanNewModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+		public string Password { get; set; }
+		public string Specify { get; set; }
+		public DateTime BeginTime { get; set; }
+		public DateTime EndTime { get; set; }
+		public IDictionary<string, bool> DaysInWeek { get; set; }
+		public int? DaysOfLeave { get; set; }
+		public IDictionary<int, int> DaysOfLeaveOptions { get; set; }
+	}
+}
