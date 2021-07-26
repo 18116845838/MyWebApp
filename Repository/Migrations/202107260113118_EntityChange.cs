863d1d8047eeb0efb745a79d1300996af337b8ee
2811733392@qq.com <2811733392@qq.com>
2021-07-26 09:16:46 +0800

    EF对Entity进行更新

---
+++ Repository/Migrations/202107260113118_EntityChange.cs
@@ -0,0 +1,22 @@
+﻿namespace Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EntityChange : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.Arcticles", newName: "Articles");
+            AddColumn("dbo.Users", "CreateTime", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Articles", "CreateTime", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Articles", "CreateTime");
+            DropColumn("dbo.Users", "CreateTime");
+            RenameTable(name: "dbo.Articles", newName: "Arcticles");
+        }
+    }
+}
