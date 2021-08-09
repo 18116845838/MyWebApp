95dd76accfb9d374c2294d75cbcc9885df49f443
2811733392@qq.com <2811733392@qq.com>
2021-08-09 17:49:29 +0800

    重新搭建架构，添加架构空项目

---
+++ UI/Controllers/NewRegisterController.cs
@@ -0,0 +1,25 @@
+﻿using NewViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace UI.Controllers
+{
+    public class NewRegisterController : Controller
+    {
+        // GET: NewRegister
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(RegisterModel model)
+        {
+
+            return View();
+        }
+    }
+}
